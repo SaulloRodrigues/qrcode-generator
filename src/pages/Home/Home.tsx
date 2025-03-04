df1751b63336076c705a75dbcc9d23a4e713080e
Saullo Rodrigues <thesaullodev@gmail.com>
2025-03-04 12:19:41 -0300

    adjust: translations.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from '@/pages/Home/Home.module.css';
+import { useTranslation } from 'react-i18next';
 const Home: React.FC = () => {
+    const { t } = useTranslation();
+
     return (
         <div>
-            <h1>Home</h1>
-            <p>This is the Home of the QR Code Generator application.</p>
+            <h1>{t("homePage.main.title")}</h1>
         </div>
     );
 };
