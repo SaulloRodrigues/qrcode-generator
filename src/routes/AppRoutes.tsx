8fc8cb5fc7d151a25a9515d1099c241fc51d3afb
Saullo Rodrigues <thesaullodev@gmail.com>
2025-03-15 18:30:15 -0300

    remove: Delete Contact page (using btn contact); update routing to reflect changes.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '@/layouts/MainLayout';
 import Home from '@/pages/Home/Home';
 import About from '@/pages/About/About';
-import Contact from '@/pages/Contact/Contact';
 
 const AppRoutes = createBrowserRouter([
     {
@@ -11,7 +10,6 @@ const AppRoutes = createBrowserRouter([
         children: [
             { index: true, element: <Home /> },
             { path: 'about', element: <About /> },
-            { path: 'contact', element: <Contact /> },
         ],
     },
     {
